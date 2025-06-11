b6b64c1a4405401820cdafd615108e0163aafeee
Daniel Kaufman <danielkaufmanpr@gmail.com>
2025-06-11 22:42:43 +0100

    Added 2023 experimental cycle dataset, bulk density data, and WatchDog weather data
    Joined bulk density data by nearest sampling event and pile,
    
    Merged WatchDog station weather data by nearest timestamp,
    
    Next: add EC tower data (U*, heat flux), and turning event variables.

---
+++ 00_setup_project.R
@@ -0,0 +1,7 @@
+# this script sets up the folder structure for the project 
+
+if(!dir.exists("data_raw")) dir.create("data_raw")
+if(!dir.exists("data_clean")) dir.create("data_clean")
+if(!dir.exists("scripts")) dir.create("scripts")
+if(!dir.exists("outputs")) dir.create("outputs")
+if(!dir.exists("docs")) dir.create("docs")
